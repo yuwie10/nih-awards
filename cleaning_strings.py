bc1a2e9b24af252a30f200fe8285cc4df93b1034
Yuwen <yuwie10@gmail.com>
2017-06-07 06:05:11 -0400

    tfidf, kmeans clustering, visualizations

--- cleaning_strings.py
+++ cleaning_strings.py
@@ -1,27 +1,5 @@
 import pandas as pd
 
-def get_dtypes(df, cols_list, dtype1 = float, dtype2 = str):
-    '''
-    Get dtypes for importing a dataframe.
-
-    Input:
-    df: dataframe to get columns from
-    cols_list: list of columns of a specific dtype
-    dtype1: dtype of columns in cols_list, default = float
-    dtype2: dtype of other columns, default = str
-
-    Output:
-    dtypes: dictionary of {column_names:dtype}
-    '''
-
-    col_dtype1 = df[cols_list].columns
-    col_dtype2 = df.drop(col_dtype1, axis = 1)
-
-    dict1 = {key: dtype1 for key in col_dtype1}
-    dict2 = {key: dtype2 for key in col_dtype2}
-
-    return {**dict1, **dict2}
-
 def strip_df(df, *args):
     '''
     
@@ -54,3 +32,12 @@ def strip_series(df, col_list, strip = '(contact)'):
     for col in col_list:
         df[col] = df[col].str.strip(strip)
     return df
+
+def list_to_string(list_of_strings):
+    '''
+    Converts a list of lists of strings to 
+    a list of strings.
+    '''
+    for i in range(len(list_of_strings)):
+        list_of_strings[i] = ''.join(list_of_strings[i]).strip()
+    return list_of_strings
