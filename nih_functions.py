def861d14311525f1013d0173ce2fb5dd2fe9cd5
Yuwen <yuwie10@gmail.com>
2017-06-08 22:28:19 -0400

    creating features for predictive modeling

--- nih_functions.py
+++ nih_functions.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import numpy as np
 
 def get_dtypes(df, cols_list, dtype1 = float, dtype2 = str):
     '''
@@ -41,6 +42,40 @@ def view_col_info(csv, cols_list = None, col_name = 'column_name'):
 		for col in cols_list:
 			descriptions.append(df.ix[df[col_name] == col])
 		return pd.concat(descriptions)
+
+def sr_transformation(df, card_var, response_col):
+    '''
+    Transform high cardinal data to a continuous variable to use in predictive model:
+    
+    supervised ratio of level = #times level is in response group / total in response group
+    
+    Inputs: 
+    Output: 
+    '''
+
+    #creates a dataframe where counts of response is grouped by the cardinal variable
+    #fills missing values with 0
+    counts = pd.DataFrame(df.groupby([card_var, response_col]).size(), columns = ['counts'])
+    counts = counts.unstack('funding_group').fillna(0).stack()
+
+    #list of distinct values of cardinal variable
+    #excluding nulls
+    unique_card = df[card_var].dropna().unique().tolist()
+
+    #total counts within each value of the cardinal variable
+    value_counts = df[card_var].value_counts()
+
+    #list of response category values
+    response_groups = df[response_col].unique().tolist()
+    
+    df_ratios = pd.DataFrame(columns = ['low', 'med-low', 'med-high', 'high']) #response_groups
+    for card in unique_card:
+    	ratios = {}
+    	for response in response_groups:
+    		ratios[response] = (counts.loc[card, response] / value_counts[card]).item()
+    	df_ratios.loc[card] = ratios
+
+    return df_ratios
 
 
 
